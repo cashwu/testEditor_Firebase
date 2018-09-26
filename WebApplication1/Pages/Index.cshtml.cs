4efd199f0760ffbb157bf68a313949d2355048e6
Cash Wu <cash@cashwu.com>
2018-09-26 15:31:11 +0800

    medium editor

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,16 +1,53 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
+using Firebase.Storage;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Firebase.Auth;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public string ImgUrl { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (Request.Form.Files.Count > 0)
+            {
+                var file = Request.Form.Files[0];
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyBDbJVPbvfQRe6IbD2K5yHSjr74_YbSJs8"));
+                    var a = await auth.SignInWithEmailAndPasswordAsync("cash@cashwu.com", "E0F31815-1D25-4D98-80C4-629332F1B8F6");
+                    ImgUrl = await new FirebaseStorage("website-cashwu.appspot.com", new FirebaseStorageOptions
+                                   {
+                                       AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                                       ThrowOnCancel = true
+                                   })
+                                   .Child("data")
+                                   .Child(file.FileName)
+                                   .PutAsync(ms);
+                }
+            }
+
+            return new JsonResult(new
+            {
+                error = false,
+                files = new[]
+                {
+                    new
+                    {
+                        url = ImgUrl
+                    }
+                }
+            });
         }
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -24,14 +24,8 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                    .AddRazorPagesOptions(o => o.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +43,6 @@ namespace WebApplication1
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc();
         }
