55bff3b7c7a99d6c7f981c241b5c4509c4ab31fa
Cash Wu <cash@cashwu.com>
2018-09-26 15:45:53 +0800

    modify using html file upload multiple

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -12,13 +12,22 @@ namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        public string ImgUrl { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
+        public IndexModel()
         {
-            if (Request.Form.Files.Count > 0)
+            ImgUrl = new List<string>();
+        }
+        
+        public List<string> ImgUrl { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(List<IFormFile> files)
+        {
+            if (files.Count == 0)
             {
-                var file = Request.Form.Files[0];
+                return Page();
+            }
+
+            foreach (var file in files)
+            {
                 using (var ms = new MemoryStream())
                 {
                     file.CopyTo(ms);
@@ -26,28 +35,20 @@ namespace WebApplication1.Pages
 
                     var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyBDbJVPbvfQRe6IbD2K5yHSjr74_YbSJs8"));
                     var a = await auth.SignInWithEmailAndPasswordAsync("cash@cashwu.com", "E0F31815-1D25-4D98-80C4-629332F1B8F6");
-                    ImgUrl = await new FirebaseStorage("website-cashwu.appspot.com", new FirebaseStorageOptions
-                                   {
-                                       AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
-                                       ThrowOnCancel = true
-                                   })
-                                   .Child("data")
-                                   .Child(file.FileName)
-                                   .PutAsync(ms);
+                    var url = await new FirebaseStorage("website-cashwu.appspot.com", new FirebaseStorageOptions
+                                    {
+                                        AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                                        ThrowOnCancel = true
+                                    })
+                                    .Child("data")
+                                    .Child(file.FileName)
+                                    .PutAsync(ms);
+
+                    ImgUrl.Add(url);
                 }
             }
 
-            return new JsonResult(new
-            {
-                error = false,
-                files = new[]
-                {
-                    new
-                    {
-                        url = ImgUrl
-                    }
-                }
-            });
+            return Page();
         }
     }
 }
