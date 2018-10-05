951f7ae7b72502c977a5b191b972f0ecb2ebaf73
Cash Wu <cash@cashwu.com>
2018-10-05 17:31:53 +0800

    add trumbowyg editer

---
+++ WebApplication1/Pages/Edit.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class EditModel : PageModel
+    {
+        public EditModel()
+        {
+        }
+        
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
