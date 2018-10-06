b1694f701cdf8ec84d597989357c9f4e9468dd54
Cash Wu <cash@cashwu.com>
2018-10-06 21:50:53 +0800

    add trumbowyg editer

--- WebApplication1/Pages/Edit.cshtml.cs
+++ WebApplication1/Pages/Edit.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,6 +14,27 @@ namespace WebApplication1.Pages
         public IActionResult OnGet()
         {
             return Page();
+        }
+        
+        public IActionResult OnPost(List<IFormFile> image, string alt)
+        {
+            var r = new
+            {
+                data = new
+                {
+                    link = "https://i.imgur.com/89lzkTD.png"
+                },
+                success = true
+            };
+            
+            // {
+            //     "data":{"id":"89lzkTD","title":null,"description":null,"datetime":1538831904,"type":"image\/png","animated":false,"width":960,"height":403,"size":56162,"views":0,"bandwidth":0,"vote":null,"favorite":false,"nsfw":null,"section":null,
+            //         "account_url":null,"account_id":0,"is_ad":false,"in_most_viral":false,"has_sound":false,"tags":[],"ad_type":0,"ad_url":"","in_gallery":false,"deletehash":"IgE4IqzQIAHsXxu","name":"",
+            //         "link":"https:\/\/i.imgur.com\/89lzkTD.png"},
+            //     "success":true,
+            //     "status":200
+            // } 
+            return new OkObjectResult(r);
         }
     }
 }
